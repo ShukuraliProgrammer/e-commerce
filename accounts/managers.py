f259656bd2dc70ee144626ac724137e19617f896
Shukurali Rezamonov <shukuralijob@gmail.com>
2024-05-21 18:04:02 +0500

    fix(superuser: is_active)

--- accounts/managers.py
+++ accounts/managers.py
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-
+        extra_fields.setdefault("is_active", True)
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
