f6c52f17914f125235cd73dfd18d9ae2a554727e
ShukuraliProgrammer <shukuralijob@gmail.com>
2024-07-27 21:43:32 +0500

    fix: staticfiles 2

--- core/settings/base.py
+++ core/settings/base.py
@@ -146,9 +146,9 @@ USE_TZ = True
 # STATIC URLS
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
+    BASE_DIR / 'static'
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
